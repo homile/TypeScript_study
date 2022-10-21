4cd14e0b24e03c08308adad37642cd7ab76a844d
homile <ssanho2@naver.com>
2022-10-21 22:05:59 +0900

    section2: 숫자 문자열 및 불리언 작업하기

--- Section1/9/app.js
+++ Section1/9/app.js
@@ -1,1 +1,14 @@
-console.log("Your code goes here...");
+function add(n1, n2, showResult, parase) {
+    var reuslt = n1 + n2;
+    if (showResult === true) {
+        console.log(resultPhrase + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var preintResult = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, preintResult, resultPhrase);
