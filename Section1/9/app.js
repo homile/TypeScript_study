48ca2645668d26acad55cf9312a90d468ca7b9d2
homile <ssanho2@naver.com>
2022-10-22 15:41:01 +0900

    section2: 배열 타입지정

--- Section1/9/app.js
+++ Section1/9/app.js
@@ -1,5 +1,17 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 var person = {
     name: "Maximilian",
-    age: 30
+    age: 30,
+    hobbies: ["Sports", "Cooking"]
 };
+var favoriteActivities; // 단일 타입 배열이므로 혼합 배열에선 사용하지 못한다.
+// let favoriteActivities: any[];  // 혼합 배열에서 any타입으로 받지만 자주 사용하지 않는다.
+favoriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- Section1/9/app.ts
+++ Section1/9/app.ts
@@ -5,6 +5,16 @@
 const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[]; // 단일 타입 배열이므로 혼합 배열에선 사용하지 못한다.
+// let favoriteActivities: any[];  // 혼합 배열에서 any타입으로 받지만 자주 사용하지 않는다.
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
