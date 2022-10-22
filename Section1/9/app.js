994543b1bb47194c6d243b7575190c4ac14bc885
homile <ssanho2@naver.com>
2022-10-22 15:22:52 +0900

    section2: 객체 타입지정

--- Section1/9/app.js
+++ Section1/9/app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, showResult, parase) {
-    var reuslt = n1 + n2;
-    if (showResult === true) {
-        console.log(resultPhrase + n1 + n2);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var preintResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, preintResult, resultPhrase);
+var person = {
+    name: "Maximilian",
+    age: 30
+};
+console.log(person.name);
